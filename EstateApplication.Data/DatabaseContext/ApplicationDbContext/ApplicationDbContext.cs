cba637fb516c5e35c151d7ff943a6dfd7c85c9ea
agharayee <agharayee@gmail.com>
2020-07-29 20:37:17 +0100

    Add project files.

---
+++ EstateApplication.Data/DatabaseContext/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstateApplication.Data.DatabaseContext.ApplicationDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+    }
+}
---
+++ EstateApplication.Data/DatabaseContext/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using EstateApplication.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstateApplication.Data.DatabaseContext.AuthenticationDbContext
+{
+   public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> option) : base(option)
+        {
+
+        }
+    }
+}
