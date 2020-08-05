3c43703f4f3235508a31bfde2c4a131987d57688
agharayee <agharayee@gmail.com>
2020-08-05 18:48:04 +0100

    added login and register models

--- EstateApplication.Web/Controllers/AccountController.cs
+++ EstateApplication.Web/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EstateApplication.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EstateApplication.Web.Controllers
@@ -13,14 +14,36 @@ namespace EstateApplication.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            throw new NotImplementedException();
         }
     }
 }
---
+++ EstateApplication.Web/Models/RegisterModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstateApplication.Web.Models
+{
+    public class RegisterModel
+    {
+        [DisplayName("Full Name")]
+        [Required]
+        public  string FullName { get; set; }
+
+        [DisplayName("Email Address")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DisplayName("Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Comfirm Password")]
+        [Compare(nameof (Password))]
+        public string ComfirmPassword { get; set; }
+
+    }
+}
